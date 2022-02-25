9485f93eb897acdc261f578538c968805b0c48a8
aidoka888 <bluewalnut996@gmail.com>
2022-02-24 20:20:19 -0600

    Spring boot intro

--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -4,9 +4,7 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
