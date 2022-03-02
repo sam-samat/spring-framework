508bc0c4acb92bea839bfee06cffca1605330c98
aidoka888 <bluewalnut996@gmail.com>
2022-03-01 20:40:13 -0600

    Spring MVC Controller and MVC

---
+++ Spring-07-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home.html";
+    }
+
+    @RequestMapping("welcome")
+    public String welcome(){
+        return "welcome.html";
+    }
+
+    @RequestMapping
+    public String noName(){
+    return "home.html";
+    }
+}
